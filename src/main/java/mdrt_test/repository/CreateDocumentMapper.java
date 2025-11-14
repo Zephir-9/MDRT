71e2b6f2f9f4f13a99f731758be9502ce5b48704
Zephyr <frastxini@gmail.com>
2025-11-14 16:46:32 +0300

    Компонент для транзакций

--- src/main/java/mdrt_test/repository/CreateDocumentMapper.java
+++ src/main/java/mdrt_test/repository/CreateDocumentMapper.java
@@ -1,4 +1,10 @@
 package mdrt_test.repository;
 
+import org.apache.ibatis.annotations.*;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+@Component
+@Mapper
 public interface CreateDocumentMapper {
 }
