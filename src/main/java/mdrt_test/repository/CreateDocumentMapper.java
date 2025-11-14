41cb80d8ff65ec588a09b0f34e4d0b7d0974852e
Zephyr <frastxini@gmail.com>
2025-11-14 16:49:08 +0300

    Чистка лишнего

--- src/main/java/mdrt_test/repository/CreateDocumentMapper.java
+++ src/main/java/mdrt_test/repository/CreateDocumentMapper.java
@@ -2,7 +2,6 @@ package mdrt_test.repository;
 
 import org.apache.ibatis.annotations.*;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
 
 @Component
 @Mapper
