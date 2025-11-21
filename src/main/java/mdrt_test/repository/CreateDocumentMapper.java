193b224d92c3130bebfe177867fde5f8dce92407
Zephyr <frastxini@gmail.com>
2025-11-21 18:43:22 +0300

    Методы добавления, изменения, удаления записей в таблице Master (без SQL и логирования)

--- src/main/java/mdrt_test/repository/CreateDocumentMapper.java
+++ src/main/java/mdrt_test/repository/CreateDocumentMapper.java
@@ -6,4 +6,20 @@ import org.springframework.stereotype.Component;
 @Component
 @Mapper
 public interface CreateDocumentMapper {
+    @Insert("""
+            """)
+    void addMaster(Integer docId, String docDate, Integer comment);
+
+    @Update("""
+            """)
+    void changeMaster(Integer docId, Integer newDocId, String docDate, Integer comment);
+
+    @Delete("""
+            """)
+    void deleteMaster(Integer docId);
+
+    @Select("""
+            where id = #{docId}
+            """)
+    Integer getDocumentById(Integer docId);
 }
--- src/main/java/mdrt_test/service/CreateDocumentService.java
+++ src/main/java/mdrt_test/service/CreateDocumentService.java
@@ -1,5 +1,6 @@
 package mdrt_test.service;
 
+import mdrt.openapi.model.EditResultDTO;
 import mdrt_test.repository.CreateDocumentMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,4 +9,40 @@ import org.springframework.stereotype.Service;
 public class CreateDocumentService {
     @Autowired
     CreateDocumentMapper createDocumentMapper;
+
+    public EditResultDTO addMaster(Integer docId, String docDate, Integer comment) {
+        if (createDocumentMapper.getDocumentById(docId) > 0) {
+            return new EditResultDTO(false, "Документ с номером {" + docId + "} уже существует");
+        } else {
+            createDocumentMapper.addMaster(docId, docDate, comment);
+            return new EditResultDTO(true, "Добавлен документ с номером: " + docId);
+        }
+    }
+
+    public EditResultDTO changeMaster(Integer docId, Integer newDocId, String docDate, Integer comment) {
+        if (docId.equals(newDocId)) {
+            createDocumentMapper.changeMaster(docId, newDocId, docDate, comment);
+            return new EditResultDTO(true, "Документ с номером: " + docId + " обновлён");
+        }
+        else if (createDocumentMapper.getDocumentById(newDocId) > 0) {
+            return new EditResultDTO(false, "Номер {" + newDocId + "} уже используется другим документом");
+        }
+        else if (createDocumentMapper.getDocumentById(docId) == 0) {
+            return new EditResultDTO(false, "Документа с номером {" + docId +"} не существует");
+        }
+        else {
+            createDocumentMapper.changeMaster(docId, newDocId, docDate, comment);
+            return new EditResultDTO(true, "Документ с номером: " + docId + " обновлён. Новый номер: " + newDocId);
+        }
+    }
+
+    public EditResultDTO deleteMaster(Integer docId) {
+        if (createDocumentMapper.getDocumentById(docId) == 0) {
+            return new EditResultDTO(false, "Документа с номером {" + docId +"} не существует");
+        } else {
+            createDocumentMapper.deleteMaster(docId);
+            return new EditResultDTO(true, "Документ с номером: " + docId + " удалён");
+        }
+    }
+
 }
