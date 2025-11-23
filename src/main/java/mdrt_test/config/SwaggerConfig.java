f33ebf3925b32d97cb2cfdde404807b9dc1cc22e
Zephyr <frastxini@gmail.com>
2025-11-23 14:34:00 +0300

    Конфиг для swagger

--- src/main/java/mdrt_test/config/SwaggerConfig.java
+++ src/main/java/mdrt_test/config/SwaggerConfig.java
@@ -2,17 +2,32 @@ package mdrt_test.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Arrays;
+
 @Configuration
 public class SwaggerConfig {
+    @Value("${server.port}")
+    private String port;
+    @Value("${server.servlet.context-path}")
+    private String contextPath;
+
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
                         .title("API Documentation")
                         .version("1.0")
-                        .description("API documentation for MDRT"));
+                        .description("API documentation for MDRT"))
+                .servers(Arrays.asList(new Server()
+                .url(contextPath)
+                .description("контекст api backend"),
+                        new Server()
+                                .url(String.format("http://localhost:%s%s", port, contextPath))
+                                .description("локальный сервер для разработки")));
     }
 }
